cc1a4b94c9238854c22a804e9823bdbd47d04830
lmitchell524 <lmitchell524@gmail.com>
2017-11-15 11:24:23 -0800

    started dom creation for grid layout

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 $(document).ready( initializeGame() );
 
 function initializeGame(){
-    console.log('does the dom load?');
+    var main = $('<main>');
+    var leftAside = $('<aside>').addClass('leftAside');
+    var rightAside = $('<aside>').addClass('rightAside');
+    var player1Stats = $('<div>').addClass('player1Stats');
+    var player2Stats = $('<div>').addClass('player2Stats');
+    var gameBoard = $('<section>').attr('id','gameboard');
+
+    for (var i = 0; i < 8; i++){
+        var row = $('<div>').addClass('row');
+        for(var col = 0; col < 8; col++){
+            var column = $('<div>').addClass('column');
+            (row).append(column);
+        }
+        return row;
+    }
+
+    (gameBoard).append(row);
+    (leftAside).append(player1Stats);
+    (rightAside).append(player2Stats);
+    (main).append(rightAside, gameBoard, leftAside);
+    ('body').append(main);
 }
 
 var model = {
